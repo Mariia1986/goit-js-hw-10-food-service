ced509161dc958bbb3755959d7d24b855a3edc7c
Mariia Randarevych <renij1986@gmail.com>
2020-12-02 23:23:17 +0300

    добавлена темная тема в localstorage

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import './styles.css';
 import cardsItem from './tamplates/cards.hbs';
-import  refs from './refs.js';
+import refs from './refs.js';
 import card from './menu.json';
-// const menu =document.querySelector('.menu')
-const{menu,input, body}=refs
-const menuItem=cardsItem(card);
+import theme from'./theme.js'
+const {LIGHT,DARK}= theme
+const { menu, input, body } = refs;
+const menuItem = cardsItem(card);
 
-menu.insertAdjacentHTML('afterbegin', menuItem)
+menu.insertAdjacentHTML('afterbegin', menuItem);
 
-input.addEventListener('change', changeTheme)
+input.addEventListener('change', changeTheme);
 
-function changeTheme(event){
-    if(event.target.checked){
-        body.classList.add('dark-theme') 
-        body.classList.remove('light-theme') 
-    } else {
-        body.classList.add('light-theme')
-        body.classList.remove('dark-theme') 
-       }
+function changeTheme(event) {
+  if (event.target.checked) {
+      localStorage.setItem('theme','dark')
+    body.classList.add('dark-theme');
+    body.classList.remove('light-theme');
+  } else {
+    body.classList.add('light-theme');
+    body.classList.remove('dark-theme');
+    
+  }
 }
+
