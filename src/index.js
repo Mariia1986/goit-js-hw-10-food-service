c831a309be1927ee578509b04c058294e328e6ea
Mariia Randarevych <renij1986@gmail.com>
2020-12-05 13:51:35 +0300

    сделан основной функционал

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './styles.css';
 import cardsItem from './tamplates/cards.hbs';
 import refs from './refs.js';
 import card from './menu.json';
-import theme from'./theme.js'
-const {LIGHT,DARK}= theme
+import theme from './theme.js';
+const { LIGHT, DARK } = theme;
 const { menu, input, body } = refs;
 const menuItem = cardsItem(card);
 
@@ -13,13 +13,24 @@ input.addEventListener('change', changeTheme);
 
 function changeTheme(event) {
   if (event.target.checked) {
-      localStorage.setItem('theme','dark')
+    localStorage.setItem('theme', 'dark');
     body.classList.add('dark-theme');
     body.classList.remove('light-theme');
   } else {
     body.classList.add('light-theme');
     body.classList.remove('dark-theme');
-    
+    localStorage.setItem('theme', 'light');
+  }
+  console.dir(event.target);
+}
+document.addEventListener('DOMContentLoaded', Theme);
+
+function Theme(event) {
+  const currentTheme = localStorage.getItem('theme');
+  console.log(currentTheme);
+  if (currentTheme === 'dark') {
+    body.classList.add('dark-theme');
+    body.classList.remove('light-theme');
+    input.checked = true;
   }
 }
-
